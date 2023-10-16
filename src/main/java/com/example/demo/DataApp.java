e7dd3e2e6f44c776c56ffb58beb44a0344ca708f
Sathish <sathishprithiviraj@gmail.com>
2023-10-17 00:36:43 +0530

    uri change

--- src/main/java/com/example/demo/DataApp.java
+++ src/main/java/com/example/demo/DataApp.java
@@ -1,30 +1,32 @@
 package com.example.demo;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class DataApp {
 
-     @GetMapping("/location")
+    Logger logger = LoggerFactory.getLogger(DataApp.class);
+
+     @GetMapping("/locations")
     public List<House> getAllData() {
         try(InputStream in=Thread.currentThread().getContextClassLoader().getResourceAsStream("db.json")){
         //pass InputStream to JSON-Library, e.g. using Jackson
             ObjectMapper mapper = new ObjectMapper();
             House[] houses = mapper.readValue(in, House[].class);
             List<House> lstHouses = Arrays.asList(houses);
-            System.out.println(lstHouses);
+            logger.debug("Houses size "+ lstHouses.size());
             return lstHouses;
            
         }
@@ -43,7 +45,7 @@ public class DataApp {
                 for (int i=0; i<houses.length;i++) {
                     if (houses[i].getId().equals(id)) {
                          
-                         System.out.println(houses[i].getId() + ", "+ houses[i].getCity());
+                         logger.debug("selected house- " + houses[i].getId() + ", "+ houses[i].getCity());
                         return houses[i];
                     }
                 }
